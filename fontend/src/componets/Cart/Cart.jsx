d8811e92e92b222db8fe2630604ca2c9cbec51c0
Your Name <Arkzero1235>
2024-12-24 14:51:14 +0700

    ...

--- fontend/src/componets/Cart/Cart.jsx
+++ fontend/src/componets/Cart/Cart.jsx
@@ -8,6 +8,8 @@ import { BsTrash3 } from "react-icons/bs";
 
 function Cart() {
     const [itemList, setItemList] = useState([])
+    const [total, setTotal] = useState(0)
+
     useEffect(async () => {
         try {
             const response = await axios.post(
@@ -24,11 +26,35 @@ function Cart() {
         }
     }, [])
 
+    useEffect(() => {
+        let money = 0;
+        for (let i = 0; i < itemList?.length; i++) {
+            money += itemList[i].price
+        }
+        setTotal(money)
+    }, [itemList])
+
+    async function delItem(documentId) {
+        try {
+            const response = await axios.post(
+                import.meta.env.VITE_BACKEND_URL + "/auth/removeItemCart",
+                { documentId },
+                { withCredentials: true }
+            );
+            window.alert("Thành công!")
+            window.location.reload()
+        }
+        catch (error) {
+            console.log(error);
+            alert(error.response.data.message);
+        }
+    }
+
     return (
         <div className={styles.cartContainer}>
             <HeaderNew />
             <div className={styles.cartContent}>
-                <p>Giỏ Hàng (27 sản phẩm)</p>
+                <p>Giỏ Hàng</p>
                 <div className={styles.title}>
                     <p className={styles.desc}>Thông tin</p>
                     <p className={styles.count}>Số lượng</p>
@@ -43,12 +69,15 @@ function Cart() {
                                 }}></div>
                                 <div className={styles.itemName}><p>{item.name}</p></div>
                                 <div className={styles.itemCount}><p>1</p></div>
-                                <div className={styles.itemMoney}><p>30.000VNĐ</p></div>
-                                <BsTrash3 style={{ height: '100%', cursor: 'pointer', marginLeft: '20px', marginTop: '5px' }} />
+                                <div className={styles.itemMoney}><p>{item.price}.000VNĐ</p></div>
+                                <BsTrash3 style={{ height: '100%', cursor: 'pointer', marginLeft: '20px', marginTop: '5px' }} onClick={() => { delItem(item.documentId) }} />
                             </div>
                         )
                     })}
-                    <button class={styles.button24} role="button">Mua Hàng</button>
+                    <div className={styles.purchase}>
+                        <p style={{ fontSize: '20px' }}>Thành tiền: {total}.000VNĐ</p>
+                        <button class={styles.button24} role="button">Mua Hàng</button>
+                    </div>
                 </div>
             </div>
             <Footer />
